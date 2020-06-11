6e99b7f18347269833a0724a11f63d93e2141420
Ethan <zy2120@qq.com>
2020-06-11 05:38:59 +0000

    user display page

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,15 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // user sign up
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
+// the above single sentence equals to
+/*
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
